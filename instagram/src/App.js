8dfa664265bba0e54ec957237f3cf0c44099c886
Ashley <jal9626@gmail.com>
2019-03-21 22:33:03 -0400

    HOC implemented

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,60 +1,25 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import dummyData from './dummy-data';
-// import PostContainer from './components/PostContainer/PostContainer'; 
-import SearchBar from './components/SearchBar/SearchBar';
-import PostPage from './components/PostContainer/PostPage';
-
+import PostsPage from './components/PostContainer/PostPage';
+import LoginPage from './components/Login/LoginPage';
+import withAuthenticate from './components/Authentication/withAuthenticate';
 
 class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        posts: [],
-        filteredPosts: [],
-      };
-    }
-    
-    componentDidMount() {
-      this.setState({ posts: dummyData })
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    searchPostsHandler = e => {
-      const posts = this.state.posts.filter(p => {
-        if (p.username.includes(e.target.value)) {
-          return p;
-        }
-      });
-      this.setState({ filteredPosts: posts });
-    };
-
-  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <SearchBar 
-            post={this.instagram} 
-            searchPosts={this.searchPostsHandler}
-            value={this.state.searchText}
-          />
-         {/* <PostContainer 
-           posts={
-            this.state.filteredPosts.length > 0
-             ? this.state.filteredPosts
-             : this.state.posts
-          }
-         /> */}
-        <PostPage 
-          posts={this.state.posts}
-          filteredPosts={this.state.filteredPosts}  
-            />
-          {console.log(this.state.posts)}
-        </header>
+        <ComponentFromWithAuthenticate />
       </div>
     );
   }
 }
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(LoginPage);
 
 export default App;
--- instagram/src/components/PostContainer/PostPage.js
+++ instagram/src/components/PostContainer/PostPage.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+import dummyData from '../../dummy-data';
 import PostContainer from './PostContainer'; 
+import SearchBar from '../SearchBar/SearchBar'; 
 
 
-function PostPage(props) {
-    console.log(props)
+class PostPage extends Component {
+    constructor() {
+      super();
+      this.state = {
+        posts: [],
+        filteredPosts: [],
+      };
+    }
+  
+    componentDidMount() {
+      this.setState({ posts: dummyData })
+    }
+
+    searchPostsHandler = e => {
+      const posts = this.state.posts.filter(p => {
+        if (p.username.includes(e.target.value)) {
+          return p;
+        }
+      });
+      this.setState({ filteredPosts: posts });
+    };
+
+
+  render() {
     return (
-      <div>
-          <PostContainer posts={
-            props.filteredPosts.length > 0
-             ? props.filteredPosts
-             : props.posts.posts
-          }/>
+      <div className="App">
+        <header className="App-header">
+          <SearchBar 
+            post={this.instagram} 
+            searchPosts={this.searchPostsHandler}
+            value={this.state.searchText}
+          />
+         <PostContainer 
+           posts={
+            this.state.filteredPosts.length > 0
+             ? this.state.filteredPosts
+             : this.state.posts
+          }
+         />
+         
+
+        </header>
       </div>
-    )
-} 
+    );
+  }
+}
 
 export default PostPage;
